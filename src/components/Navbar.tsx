7e2e8d7e477219d65edd87fe8aa635804c33cc0a
david-taiwo <davidtaiwo003@gmail.com>
2025-06-16 17:35:33 +0100

    installed all dependences and completed the file structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      Hello, I am the Navbar component. I will be used to navigate through the website.
+    </div>
+  )
+}
+
+export default Navbar
